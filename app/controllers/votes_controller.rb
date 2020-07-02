ea36f9cfd66f16c04e89096eb83bd5e74be181b0
FrancoRosa <km115.franco@gmail.com>
2020-07-02 00:31:15 -0500

    Add vote and unvote links at Article#Show page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -24,51 +24,47 @@ class VotesController < ApplicationController
   # POST /votes
   # POST /votes.json
   def create
-    @vote = Vote.new(vote_params)
-
-    respond_to do |format|
+    current_user = session[:current_user]
+    if Vote.where(user_id: current_user).empty?
+      @vote = Vote.new(vote_params)
+      @vote.user_id = current_user
       if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
-        format.json { render :show, status: :created, location: @vote }
+        redirect_to article_path(@vote.article_id), notice: 'Vote was successfully destroyed.'
       else
-        format.html { render :new }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
+        render :new
       end
+    else
+      update
     end
   end
 
   # PATCH/PUT /votes/1
   # PATCH/PUT /votes/1.json
   def update
-    respond_to do |format|
-      if @vote.update(vote_params)
-        format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
-        format.json { render :show, status: :ok, location: @vote }
-      else
-        format.html { render :edit }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
+    @vote = Vote.find_by(user_id: session[:current_user])
+    if @vote.update(vote_params)
+      redirect_to @vote, notice: 'Vote was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /votes/1
   # DELETE /votes/1.json
   def destroy
+    article_id = @vote.article_id
     @vote.destroy
-    respond_to do |format|
-      format.html { redirect_to votes_url, notice: 'Vote was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to article_path(article_id), notice: 'Vote was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vote
-      @vote = Vote.find(params[:id])
+      @vote = Vote.find_by(user_id: session[:current_user])
     end
 
     # Only allow a list of trusted parameters through.
     def vote_params
-      params.require(:vote).permit(:user_id, :article_id)
+      params.permit(:article_id)
     end
 end
