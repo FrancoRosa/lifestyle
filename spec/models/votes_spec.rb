e57046504203682c11eecaf3b00a527714d39915
FrancoRosa <km115.franco@gmail.com>
2020-07-07 23:11:22 -0500

    Add Validation to Votes Model

--- spec/models/votes_spec.rb
+++ spec/models/votes_spec.rb
@@ -3,32 +3,36 @@ require 'rails_helper'
 describe Vote do
   subject { Vote.new() }
 
-  let(:author) { User.create(name: "user") }
+  let!(:user) { User.create(name: "my_user") }
+  let(:article) { Article.new(title: "title", text: "text text", author: User.first, image: nil)}
+  let(:article1) { Article.new(title: "title1", text: "text text", author: User.first, image: nil)}
   
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:article) }
 
   it 'is valid with valid attributes' do
-    a = Article.new(title: "title", 
-                    text: "text", 
-                    author: author)
-
-    a.image.attach(io: File.open(Rails.root.join('public','apple-touch-icon.png')),
-            filename: 'attachment.png', 
-            content_type: 'image/png')  
-    a.save
-    subject.user = author
-    subject.article = Article.first
+    article.image.attach(io: File.open(Rails.root.join('public','apple-touch-icon.png')),
+                         filename: 'attachment.png', 
+                         content_type: 'image/png')
+    article.save
+    subject.user = user
+    subject.article = article
     expect(subject).to be_valid
   end
 
   it 'is not valid with nil values' do
-    subject.name = nil
+    subject.user = nil
+    subject.article = nil
     expect(subject).to_not be_valid
   end
 
-  it 'is not valid if user already exists' do
-    User.create(name: name)
+  it 'is not valid if user already voted before' do
+    article.image.attach(io: File.open(Rails.root.join('public','apple-touch-icon.png')),
+                         filename: 'attachment.png', 
+                         content_type: 'image/png')
+
+    a = Vote.new(user_id: user.id, article_id: article.id)
+    subject.article = article
     expect(subject).to_not be_valid
   end
 end
