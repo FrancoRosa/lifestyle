4fbb213959ca896de2e5434033410ba23ce66c2b
FrancoRosa <km115.franco@gmail.com>
2020-07-09 15:30:38 -0500

    Refactor main article view

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,18 @@
 module ArticlesHelper
+  def featured_article
+    if @featured_id.empty?
+      render 'def_article'
+    else
+      render 'top_article'
+    end
+  end
+
+  def latest_article(category)
+    if category.articles.count.positive?
+      article = category.articles.latest
+      render partial: 'top_category', locals: { category: category, article: article }
+    else
+      render partial: 'def_category', locals: { category: category }
+    end
+  end
 end
