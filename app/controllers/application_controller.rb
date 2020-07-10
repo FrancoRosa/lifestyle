ff707aaa39f26d21f1e82843f374bf3a55fa42d2
FrancoRosa <km115.franco@gmail.com>
2020-07-10 11:23:39 -0500

    Move logic to categories_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_variables
 
   def set_variables
-    @nav_categories = Category.all.order(priority: :asc).limit(4)
+    @nav_categories = Category.top(4)
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,37 @@
 module CategoriesHelper
+  def session_votes(article)
+    return if session[:current_user].nil?
+
+    vote = article.votes.find_by(user_id: session[:current_user])
+    if vote.nil?
+      link_to('<i class="far fa-thumbs-up"></i>'.html_safe,
+              votes_path(article_id: article.id), method: :post, class: 'text-grey')
+    elsif vote.article_id == article.id
+      link_to('<i class="far fa-thumbs-down"></i>'.html_safe,
+              vote_path(vote.id), method: :delete, class: 'text-orange')
+    else
+      link_to('<i class="far fa-thumbs-up"></i>'.html_safe,
+              votes_path(article_id: article.id), method: :post, class: 'text-grey')
+    end
+  end
+
+  def session_edit_destroy(article)
+    return unless article.author_id == session[:current_user]
+
+    content_tag(:span, ' | ', class: 'text-grey') +
+      content_tag(:span, link_to('Edit', edit_article_path(article), class: 'text-grey')) +
+      content_tag(:span, ' | ', class: 'text-grey') +
+      content_tag(:span, link_to('Destroy', article, method: :delete,
+                                                     data: { confirm: 'Are you sure?' }, class: 'text-orange'))
+  end
+
+  def row_selector(article, index)
+    if ((index / 2) % 2).zero?
+      content_tag(:div, image_tag(article.image), class: 'article-image') +
+        content_tag(:div, render(partial: 'article_text', locals: { article: article }), class: 'article-text')
+    else
+      content_tag(:div, render(partial: 'article_text', locals: { article: article }), class: 'article-text') +
+        content_tag(:div, image_tag(article.image), class: 'article-image')
+    end
+  end
 end
